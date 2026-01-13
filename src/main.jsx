154cfc38140d0d38f3fb2f66c3375f59bf729a3e
poorvesh <poorveshgediya@gmail.com>
2026-01-13 18:50:54 +0530

    @vercel/analytics added

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    <Analytics/>
   </StrictMode>
 );
