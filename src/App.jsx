181e95e1834d66c42f8fcc805229ceaf68d2e3cb
poorvesh <poorveshgediya@gmail.com>
2026-01-13 18:54:15 +0530

    Removed Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import GitHubProfile from "./Components/GitHubProfile";
-import RepoData from "./Components/RepoData";
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/" element={<GitHubProfile/>}/>
-      <Route path="/repositories" element={<RepoData />}/>
-    </Routes>
-      {/* <GitHubProfile /> */}
+      <GitHubProfile />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-    <Analytics/>
+    <App />
+    <Analytics />
   </StrictMode>
 );
