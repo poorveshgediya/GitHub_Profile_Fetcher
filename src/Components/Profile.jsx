1f45ef5c84af8c66c69eebd48b13a22a50f43455
poorvesh <poorveshgediya@gmail.com>
2026-03-26 14:19:03 +0530

    corrected data

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -96,7 +96,7 @@ const Profile = () => {
                 </h2>
                 <p className="text-white/80 max-[400px]:text-[0.9rem]">
                   <span>Joined </span>
-                  {/* {dataprofile.created_at.slice(0, 10) || "xxxx-xx-xx"} */}
+                  {dataprofile.created_at.slice(0, 10) || "xxxx-xx-xx"}
                 </p>
               </div>
               <p className="text-sm text-blue-500">@{dataprofile.login}</p>
